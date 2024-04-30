7ae0d6da92186de938d2fd839f8b08f67258a7e5
JuanJoseSolo <juanjose.solorzano.c@gmail.com>
2024-04-30 00:10:44 -0500

    Created: <repo IA> - new git-repo.

---
+++ perceptron.py
@@ -0,0 +1,80 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+"""
+{{{
+    a line is defined by:
+        y=mx -> where m is the slope (gradient) and the x is the independient variable of the function.
+        m=(p2/p1)*x
+        a pint P' = (p'1,p'2) is on the line if the following condition is fulfiled:
+        m*(p'1)-p'2 = 0
+
+}}}
+"""
+folder='images'
+try:
+    os.mkdir(folder)
+except:
+    pass
+
+def line():
+#{{{
+    # x vector
+    x = list(range(10))
+    p1 = 3.5 # coordinate a of a point.
+    p2 = 1.0 # coordinate b of a point.
+    plt.plot(p1,p2,'ro')
+    px = 1.1# coordinate a of a point.
+    py = 3.9# coordinate b of a point.
+    plt.plot(px,py,'bo')
+    # the point in the line   
+    P1_ = 4# coordinate a of a point.
+    P2_ = 4.5 # coordinate a of a point.
+    
+    m = P2_/P1_ # slope
+    #y = m*x line ecuation
+    y = []
+    for val in x: #fill the y vector with the slop.
+        y.append(val*m)
+
+    plt.plot(x,y) #plot the line
+    plt.savefig('{}/line'.format(folder))
+#}}}
+
+class Perceptron:
+#{{{
+    """
+    @Public_class: This class is an example of the use of simple perceptron.
+    @Attr:
+        -
+    @Usage:
+    @theroy:
+        learning rate: 
+    """
+    def __init__(self,learning_rate,epochs):
+        self.weights = 0
+        self.bias = 0
+        self.learning_rate = learning_rate
+        self.epochs = epochs
+
+    def activation(self,z):
+        return np.heaviside(z,0)
+    
+    def fit(self,x,y):
+        n_features = x.shape[1]
+        self.weights = np.zeros((n_features))
+        
+        for epoch in range(self.epochs):
+            for i in range(len(x)):
+                z = np.dot(x,self.weights) + self.bias
+                y_pred = self.activation(z)
+                self.weights = self.weights + self.learning_rate * (y[i] - y_pred[i])*x[i]
+                self.bias = self.bias + self.learning_rate * (y[i] - y_pred[i])
+
+        return self.weights, self.bias
+
+    def predict(self,x):
+        z = np.dot(x,self.weights) + self.bias
+        return self.activation(z)
+#}}}
+
---
+++ test_perceptron.py
@@ -0,0 +1,45 @@
+import numpy as np
+from sklearn.datasets import load_iris
+from sklearn.model_selection import train_test_split
+from perceptron import Perceptron,line,folder
+import matplotlib.pyplot as plt
+import os
+
+if not os.path.isdir(folder):
+    os.mkdir(folder)
+
+
+def get_data_info():
+    iris = load_iris()
+    x = iris.data[:,(0,1)]
+    y = (iris.target == 0).astype(np.int_)
+    x_train,x_test,y_train,y_test = train_test_split(x,y,test_size=0.5,random_state=42)
+    for axis,group in zip(x_train,y_train):
+        if group:
+            color = 'ro'
+        else:
+            color = 'bo'
+        plt.plot(axis[0],axis[1],color)
+    plt.savefig('{}/training_data'.format(folder))
+    plt.clf()
+    for axis in x_test:
+        plt.plot(axis[0],axis[1],'ro')
+        plt.savefig('{}/test_data'.format(folder))
+
+    return x_train,x_test,y_train,y_test
+
+if __name__ == '__main__':
+    x_train,x_test,y_train,y_test = get_data_info()
+    perceptron = Perceptron(0.001,100)
+    perceptron.fit(x_train,y_train)
+    y_pred = perceptron.predict(x_test)
+    plt.clf()
+    for axis,group in zip(x_test,y_pred):
+        if group:
+            color = 'ro'
+        else:
+            color = 'bo'
+        plt.plot(axis[0],axis[1],color)
+        plt.savefig('{}/prediction'.format(folder))
+    plt.clf()
+    line()
